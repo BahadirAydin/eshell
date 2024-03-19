dd6ba274ddfeb981861c429da8369c9e13de4180
BahadirAydin <bahadiraydin1923@gmail.com>
2024-03-19 18:25:54 +0300

    free the input after using and some loop modifications

--- main.cpp
+++ main.cpp
@@ -19,14 +19,20 @@ auto main() -> int {
     parsed_input input;
     char *line = nullptr;
     while (line == nullptr || strcmp(line, "quit") != 0) {
+    while (true) {
         line = readstdin("/> ");
+        // NOTE: i assumed no need to call parse_line
+        // if line is null, it means EOF
+        if (!line || strcmp(line, "quit") == 0) {
+            break;
+        }
         int res = parse_line(line, &input);
         if (res == -1) {
             std::cout << "Error parsing input\n";
             exit(1);
         }
-        // for debugging
         // pretty_print(&input);
+        free_parsed_input(&input);
     }
     return 0;
 }

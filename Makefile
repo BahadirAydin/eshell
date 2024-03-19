905082ed643fad740ad8e8ae632fccf0201caa4f
BahadirAydin <bahadiraydin1923@gmail.com>
2024-03-19 17:05:58 +0300

    rename to hw1

---
+++ Makefile
@@ -0,0 +1,3 @@
+all:
+	gcc -c -o parser.o parser.c
+	g++ -o eshell *.cpp parser.o -std=c++14
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include "parser.h"
+
+auto main() -> int {
+    parsed_input input;
+    char *line = nullptr;
+    parse_line(line, &input);
+    return 0;
+}
